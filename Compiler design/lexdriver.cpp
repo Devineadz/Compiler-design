43de13b60ef8dbbc33a34dc2685339c45a1c5d5e
Diana Zitting-Rioux <44038147+Devineadz@users.noreply.github.com>
2021-02-11 01:32:01 -0500

    Added functionality

--- Compiler design/lexdriver.cpp
+++ Compiler design/lexdriver.cpp
@@ -10,40 +10,29 @@ string tokenHolder = "";
 bool finalState = false;
 bool needsBacking = false;
 int lineCounter = 0;
-
-int main() {
-	fstream srcFile;
-	srcFile.open("test.text", ios::in);
-	if (srcFile.is_open()) {
-		string textLine;
-		while (getline(srcFile, textLine)) {
-			fileRows.push_back(textLine);
-		}
-	}
-
-	while (!fileRows.empty()) {
-		string token = nextToken();
-		if (token == "error") {
-			// write to errorlex
-		}
-		else {
-			// write to lex
-		}
-	}
-	
-	return 0;
-}
+string nextLine = ""; 
+bool isLast = false;
 
 char nextChar() {
-	// if not in vocab - return non valid vocab error
-	//	return nextchar
-	// lexeme = lexeme + newchar;
-	return 'c';
+		while (nextLine == "") {
+			lineCounter++;
+			nextLine = fileRows.front();
+			fileRows.erase(fileRows.begin()); 
+		}
+		char c = nextLine.at(0);
+		lexeme = lexeme + c;
+		nextLine.erase(0, 1);
+		if (nextLine == "") {
+			isLast = true;
+		}
+		return c;
 }
 
 bool isFinalState() {
-	if (finalState == true)
+	if (finalState == true) {
+		isLast = false;
 		return true;
+	}
 	else
 		return false;
 }
@@ -60,7 +49,8 @@ bool backUp() {
 }
 
 void backUpChar() {
-	// backup one char
+	nextLine = lexeme.at(lexeme.length() - 1) + nextLine;
+	lexeme.erase(lexeme.length() - 1);
 }
 
 
@@ -69,103 +59,159 @@ int table(int state, char lookup) {
 	switch (state) {
 	case 1: //A
 		if ((lookup >= 'a' && lookup <= 'z') || (lookup >= 'A' && lookup <= 'Z')) {
-			state = 4;
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "id";
+			}
+			else
+				state = 4;
 		}
-		if (lookup > 0 && lookup < 10)
-			state = 2;
-		if (lookup == 0)
+		else if (lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "int";
+			}
+			else
+				state = 2;
+		}
+		else if (lookup == '0') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "int";
+			}
 			state = 3;
-		if (lookup == '"')
-			state = 5;
-		if (lookup == '+') {
+		}
+		else if (lookup == '"') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "stringlit";
+			}
+			else
+				state = 5;
+		}
+		else if (lookup == '+') {
 			finalState = true;
 			tokenHolder = "plus"; 
 		}
-		if (lookup == '-') {
+		else if (lookup == '-') {
 			finalState = true;
 			tokenHolder = "minus"; 
 		}
-		if (lookup == '_') { // error, cannot start with underscore
+		else if (lookup == '_') { // error, cannot start with underscore
 			finalState = true;
 			tokenHolder = "error";
 		}
-		if (lookup == '.') {
+		else if (lookup == '.') {
 			finalState = true;
 			tokenHolder = "dot"; 
 		}
-		if (lookup == '=')
-			state = 28;
-		if (lookup == '(') {
+		else if (lookup == '=') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "eq";
+			}
+			else
+				state = 28;
+		}
+		else if (lookup == '(') {
 			finalState = true;
 			tokenHolder = "openpar"; 
 		}
-		if (lookup == ')') {
+		else if (lookup == ')') {
 			finalState = true;
 			tokenHolder = "closepar"; 
 		}
-		if (lookup == '{') {
+		else if (lookup == '{') {
 			finalState = true;
 			tokenHolder = "opencubr"; 
 		}
-		if (lookup == '}') {
+		else if (lookup == '}') {
 			finalState = true;
 			tokenHolder = "closecubr"; 
 		}
-		if (lookup == '[') {
+		else if (lookup == '[') {
 			finalState = true;
 			tokenHolder = "opensqbr"; 
 		}
-		if (lookup == ']') {
+		else if (lookup == ']') {
 			finalState = true;
 			tokenHolder = "closesqbr"; 
 		}
-		if (lookup == ';') {
+		else if (lookup == ';') {
 			finalState = true;
 			tokenHolder = "semi"; 
 		}
-		if (lookup == ',') {
+		else if (lookup == ',') {
 			finalState = true;
 			tokenHolder = "comma";
 		}
-		if (lookup == '|') {
+		else if (lookup == '|') {
 			finalState = true;
 			tokenHolder = "or"; 
 		}
-		if (lookup == '&') {
+		else if (lookup == '&') {
 			finalState = true;
 			tokenHolder = "and";
 		}
-		if (lookup == '!') {
+		else if (lookup == '!') {
 			finalState = true;
 			tokenHolder = "not"; 
 		}
-		if (lookup == '?') {
+		else if (lookup == '?') {
 			finalState = true;
 			tokenHolder = "qmark";
 		}
-		if (lookup == '=') {
-			state = 28;
+		else if (lookup == '<') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "lt";
+			}
+			else
+				state = 29;
 		}
-		if (lookup == '<') {
-			state = 29;
+		else if (lookup == '>') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "gt";
+			}
+			else
+				state = 30;
 		}
-		if (lookup == '>') {
-			state = 30;
+		else if (lookup == '*') {
+				finalState = true;
+				tokenHolder = "mult";
 		}
-		if (lookup == '*') {
-			state = 31;
-		}
-		if (lookup == '/') {
+		else if (lookup == '/') {
 			state = 32;
 		}
-		if (lookup == ':') {
-			state = 33;
+		else if (lookup == ':') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "colon";
+			}
+			else
+				state = 33;
+		}
+		else if (lookup == ' ') {
+			finalState = true;
+			tokenHolder = "empty";
+		}
+		else {
+			finalState = true;
+			tokenHolder = "invalid character";
 		}
 		return state;
 		break;
 	case 2: // B
-		if (lookup > -1 || lookup < 10) {}
-		if (lookup == '.') {
+		if (lookup == '0' || lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9') {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "intnum";
+			}
+			else
+				state = 2;
+		}
+		else if (lookup == '.') {
 			state = 6;
 		}
 		else {
@@ -189,7 +235,12 @@ int table(int state, char lookup) {
 		return state;
 		break;
 	case 4: // D
-		if ((lookup >= 'a' && lookup <= 'z') || (lookup >= 'A' && lookup <= 'Z') || (lookup > -1 || lookup < 10) || (lookup == '_')) {}
+		if ((lookup >= 'a' && lookup <= 'z') || (lookup >= 'A' && lookup <= 'Z') || lookup == '0' || lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9' || (lookup == '_')) {
+			if (isLast == true) {
+				finalState = true;
+				tokenHolder = "id";
+			}
+		}
 		else {
 			finalState = true;
 			needsBacking = true;
@@ -199,8 +250,8 @@ int table(int state, char lookup) {
 		return state;
 		break;
 	case 5: // E
-		if ((lookup >= 'a' && lookup <= 'z') || (lookup >= 'A' && lookup <= 'Z') || (lookup > -1 || lookup < 10) || (lookup == ' ')) {}
-		if (lookup == '"') {
+		if ((lookup >= 'a' && lookup <= 'z') || (lookup >= 'A' && lookup <= 'Z') || lookup == '0' || lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9' || (lookup == ' ')) {}
+		else if (lookup == '"') {
 			finalState = true;
 			tokenHolder = "stringlit"; 
 			state = 1;
@@ -213,7 +264,7 @@ int table(int state, char lookup) {
 		return state;
 		break;
 	case 6: // F
-		if (lookup > -1 || lookup < 10) {
+		if (lookup == '0' || lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9') {
 			state = 8;
 		}
 		else {
@@ -224,11 +275,11 @@ int table(int state, char lookup) {
 		return state;
 		break;
 	case 8: // H
-		if (lookup > 0 || lookup < 10) {}
-		if (lookup == 0) {
+		if ( lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9') {}
+		else if (lookup == '0') {
 			state = 9;
 		}
-		if (lookup == 'e') {
+		else if (lookup == 'e') {
 			state = 10;
 		}
 		else {
@@ -240,10 +291,10 @@ int table(int state, char lookup) {
 		return state;
 		break;
 	case 9: // I
-		if (lookup > 0 || lookup < 10) {
+		if (lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9') {
 			state = 8;
 		}
-		if (lookup == 0) {}
+		else if (lookup == '0') {}
 		else {
 			finalState = true;
 			tokenHolder = "error";
@@ -263,7 +314,7 @@ int table(int state, char lookup) {
 		return state;
 		break;
 	case 11: // K
-		if (lookup > -1 || lookup < 10) {
+		if (lookup == '0' || lookup == '1' || lookup == '2' || lookup == '3' || lookup == '4' || lookup == '5' || lookup == '6' || lookup == '7' || lookup == '8' || lookup == '9') {
 			finalState = true;
 			needsBacking = false;
 			tokenHolder = "floatnum";
@@ -304,7 +355,7 @@ int table(int state, char lookup) {
 			state = 1;
 		}
 		// <>
-		if (lookup == '>') {
+		else if (lookup == '>') {
 			finalState = true;
 			needsBacking = false;
 			tokenHolder = "noteq"; 
@@ -356,17 +407,11 @@ int table(int state, char lookup) {
 	case 32: // N5
 		// /*
 		if (lookup == '*') {
-			finalState = true;
-			needsBacking = false;
-			tokenHolder = "blockcmt"; 
-			state = 1;
+			state = 39;
 		}
 		// //
-		if (lookup == '/') {
-			finalState = true;
-			needsBacking = false;
-			tokenHolder = "inlinecmt"; 
-			state = 1;
+		else if (lookup == '/') {
+			state = 40;
 		}
 		// /
 		else {
@@ -393,6 +438,35 @@ int table(int state, char lookup) {
 		}
 		return state;
 		break;
+	case 39: // block comment
+		if (lookup == '*')
+			state = 47;
+		return state;
+		break;
+	case 40: //inline comment
+		if (isLast == true) {
+			finalState = true;
+			needsBacking = false;
+			tokenHolder = "inlinecmt";
+			state = 1;
+		}
+		return state;
+		break;
+	case 47: // end comment
+		if (lookup == '/') {
+			finalState = true;
+			needsBacking = false;
+			tokenHolder = "blockcmt";
+			state = 1;
+		}
+		else {
+			finalState = true;
+			needsBacking = false;
+			tokenHolder = "error";
+			state = 1;
+		}
+		return state;
+		break;
 	default:
 		cout << "No such case";
 	}
@@ -406,9 +480,125 @@ string nextToken() {
 		state = table(state, lookup);
 		if (isFinalState())
 			token = createToken();
-		if (backUp())
+		if (backUp()) {
 			backUpChar();
-	} while (token != "");
+		}
+	} while (token == "");
 	return token;
 }
 
+string checkReservedWords(string token) {
+	string newToken = token;
+	if (lexeme == "if") {
+		newToken = "if";
+	}
+	else if (lexeme == "private") {
+		newToken = "private";
+	}
+	else if (lexeme == "public") {
+		newToken = "public";
+	}
+	else if (lexeme == "func") {
+		newToken = "func";
+	}
+	else if (lexeme == "then") {
+		newToken = "then";
+	}
+	else if (lexeme == "read") {
+		newToken = "read";
+	}
+	else if (lexeme == "write") {
+		newToken = "write";
+	}
+	else if (lexeme == "class") {
+		newToken = "class";
+	}
+	else if (lexeme == "var") {
+		newToken = "var";
+	}
+	else if (lexeme == "else") {
+		newToken = "else";
+	}
+	else if (lexeme == "integer") {
+		newToken = "integer";
+	}
+	else if (lexeme == "main") {
+		newToken = "main";
+	}
+	else if (lexeme == "inherits") {
+		newToken = "inherits";
+	}
+	else if (lexeme == "string") {
+		newToken = "string";
+	}
+	else if (lexeme == "while") {
+		newToken = "while";
+	}
+	else if (lexeme == "void") {
+		newToken = "void";
+	}
+	else if (lexeme == "continue") {
+		newToken = "continue";
+	}
+	else if (lexeme == "float") {
+		newToken = "float";
+	}
+	else if (lexeme == "break") {
+		newToken = "break";
+	}
+	else if (lexeme == "return") {
+		newToken = "return";
+	}
+	return newToken;
+}
+
+int main() {
+	fstream srcFile;
+	int currentRow = 0;
+	srcFile.open("test.text", ios::in);
+	if (srcFile.is_open()) {
+		string textLine;
+		while (getline(srcFile, textLine)) {
+			fileRows.push_back(textLine);
+		}
+	}
+
+	while (!fileRows.empty()) {
+		string token = nextToken();
+		if (currentRow < lineCounter) {
+			cout << "\n";
+			currentRow++;
+		}
+		if (token == "error" || token == "invalid character") {
+			cout << "[" << token << ", " << lexeme << ", " << lineCounter << "]";
+		}
+		else {
+			if (token != "empty") { // check the list of reserved words and rename token if found in list
+				if (token == "id") {
+					token = checkReservedWords(token);
+				}
+				if (token == "stringlit") { //remove "" from strings
+					lexeme.erase(0, 1);
+					lexeme.erase(lexeme.length() - 1, lexeme.length());
+				}
+				if (token == "inlinecmt") { // remove // from inline comments
+					lexeme.erase(0, 1);
+					lexeme.erase(0, 1);
+				}
+				if (token == "blockcmt") { // remove /**/ from block comments
+					lexeme.erase(0, 1);
+					lexeme.erase(0, 1);
+					lexeme.erase(lexeme.length() - 1, lexeme.length());
+					lexeme.erase(lexeme.length() - 1, lexeme.length());
+				}
+				cout << "[" << token << ", " << lexeme << ", " << lineCounter << "]";
+			}
+		}
+		tokenHolder = "";
+		finalState = false;
+		needsBacking = false;
+		lexeme = "";
+	}
+
+	return 0;
+}
